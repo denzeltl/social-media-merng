901d537c9c0846ffd98479ef65b2e578aa1148dc
Denzel <denzeltlee@gmail.com>
2020-11-13 23:48:49 +0800

    Finish create/delete post functions

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const typeDefs = require('./graphql/typeDefs');
 const resolvers = require('./graphql/resolvers');
 const { MONGODB } = require('./config');
 
-const server = new ApolloServer({ typeDefs, resolvers });
+const server = new ApolloServer({ typeDefs, resolvers, context: ({ req }) => ({ req }) });
 
 mongoose
     .connect(MONGODB, { useNewUrlParser: true, useUnifiedTopology: true })
